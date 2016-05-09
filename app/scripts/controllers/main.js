4ba8a433173a28820e4f3935465102bbca0434f3
Sunny Yuen <yuens1002@gmail.com>
2016-05-09 12:45:17 -0400

    project scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cookbriteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cookbriteApp
+ */
+angular.module('cookbriteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
